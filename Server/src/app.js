e20f9b99fee2294b7a159df18327818adba14831
Zihad Bepari <2215151139@uits.edu.bd>
2026-03-07 22:33:39 +0600

    initiale setup

---
+++ Server/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cokkieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+dotenv.config({path: "./.env"});
+const app = express();
+
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
+app.use(cokkieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use(fileUpload({
+  tempFileDir: "./upload",
+  useTempFiles: true
+}))
+
+app.get("/", (req, res) => {
+  res.send("Ai E-Commerce API is running");
+});
+
+export default app;
